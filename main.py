1e2f0c3b63ef5593d82d6f75a202d4cdc42f19fc
volkswg <volkswg@hotmail.com>
2022-01-31 00:24:43 +0700

    refactor main script

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ s = sched.scheduler(time.time, time.sleep)
 
 event_trigger_time = 60
 
+# waking state
+# 0 = common
+# 1 = rare
+# 2 = super rare
+waking_hero_state = 0
 
-common_started_flag = False
-rsr_started_flag = False
 
 wait_for_stamina_flag = False
 
@@ -22,79 +25,40 @@ min_wait_stamina_counter = 0
 
 def main_event_loop():  # this will trigger every 1 mins
     #  global vaiable declare
-    global common_started_flag
-    global rsr_started_flag
     global wait_for_stamina_flag
     global min_wait_stamina_counter
 
-    # check error occur every trigger time
-    bomb_error = bomb_controller.check_error_occur()
-    if bomb_error:
-        # bomb_controller.error_handling()
-        print('Error True')
-    else:
-        print('Continue Process')
+    global waking_hero_state
 
-    # print('wait_for_stamina_flag', wait_for_stamina_flag)
+    hero_status = bomb_controller.full_process_auto(
+        wait_for_stamina_flag, waking_hero_state)
+
+    print(hero_status)
+    waking_hero_state = hero_status['next_state']
+
     if wait_for_stamina_flag:
         min_wait_stamina_counter += 1
-        bomb_controller.re_enter_map()
-        print(min_wait_stamina_counter)
+        print(
+            f'Wait({min_wait_stamina_counter}/{int(min_time_full_stamina/event_trigger_time)})')
         if min_wait_stamina_counter >= min_time_full_stamina/event_trigger_time:
             wait_for_stamina_flag = False
             min_wait_stamina_counter = 0
 
-        return
-
-    # print('this is main loop')
-    all_rest_result = bomb_controller.check_all_rest()
-    print('All Sleep', all_rest_result)
-    print('common', common_started_flag)
-    print('rsr', rsr_started_flag)
-
-    if all_rest_result:
-        # check all empty stamina then wait for stamina
-        if common_started_flag is True and rsr_started_flag is True:
-            common_started_flag = False
-            rsr_started_flag = False
-            wait_for_stamina_flag = True
-
-        if wait_for_stamina_flag:
-            min_wait_stamina_counter += 1
-            bomb_controller.re_enter_map()
-            print(min_wait_stamina_counter)
-            if min_wait_stamina_counter == min_time_full_stamina/event_trigger_time:
-                wait_for_stamina_flag = False
-                min_wait_stamina_counter = 0
-            return
-
-        if common_started_flag:
-            bomb_controller.wake_hero(
-                all_hero_count=15, hero_rarity=['rare', 'superrare'])
-            # common_started_flag = False
-            rsr_started_flag = True
-        elif rsr_started_flag:
-            bomb_controller.wake_hero(
-                all_hero_count=15, hero_rarity=['common'])
-            common_started_flag = True
-            # rsr_started_flag = False
-        else:
-            bomb_controller.wake_hero(
-                all_hero_count=15, hero_rarity=['common'])
-            common_started_flag = True
+    if waking_hero_state == 3 and hero_status['is_all_rest']:
+        wait_for_stamina_flag = True
 
 
 def main():
-    global common_started_flag
-    global rsr_started_flag
+    global waking_hero_state
 
     # before main loop
     # arrange bomb screen
     bombcrypto_wins = screen_arrange.get_bomb_wins_list()
     screen_arrange.arrange_bomb_wins(bombcrypto_wins)
     bomb_controller.enter_hunting()
-    bomb_controller.wake_hero(all_hero_count=15, hero_rarity=['common'])
-    common_started_flag = True
+    bomb_controller.wake_hero(
+        all_hero_count=15, hero_rarity=['common'])
+    waking_hero_state += 1
     while True:
         s.enter(event_trigger_time, 1, main_event_loop)
         s.run()
